45bdd80ee1a935a005ba780e316e658389c8293a
Darya Yarovaya <yarovayadarya@mail.ru>
2021-07-27 14:39:54 +0300

    Initial Commit

---
+++ TrafficLightHW_3.1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TrafficLightHW_3.1
+//
+//  Created by Darya Yarovaya (RU) on 27.07.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TrafficLightHW_3.1/TrafficLightHW_3_1App.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightHW_3_1App.swift
+//  TrafficLightHW_3.1
+//
+//  Created by Darya Yarovaya (RU) on 27.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightHW_3_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
